6c266fd8c4499e1e858cf5e82af1af28f261ca6b
jeongsol-kim <wjdtfh3927@gmail.com>
2023-02-26 17:43:44 +0900

    create image list for publications

--- src/components/publication/Publication.js
+++ src/components/publication/Publication.js
@@ -1,12 +1,42 @@
 
+import { ImageList, ImageListItem, ImageListItemBar } from "@mui/material";
 import {Component, Fragment} from "react";
 
 
 const PublicationBlock  = () => (
     <section className="section">
-        <p>Publications</p>
+        <div className="container is-max-desktop">
+            <div className="columns is-centered">
+                <div className="column is-four-fifths">
+        <ImageList cols={1}>
+            {itemData.map((item) => (
+                <ImageListItem key={item.img}>
+                    <img></img>
+                <ImageListItemBar
+                    title={item.title}
+                    subtitle={<span>{item.subtitle}</span>}
+                    position="below"/>
+                </ImageListItem>
+            ))}
+        </ImageList>
+        </div>
+        </div>
+        </div>
     </section>
 )
+
+const itemData = [
+    {
+        img: '',
+        title: 'Diffusion Posterior Sampling for General Noisy Inverse Problems',
+        subtitle: 'ICLR 2023, spotlight'
+    },
+    {
+        img: '',
+        title: 'Diffusion Posterior Sampling for General Noisy Inverse Problems',
+        subtitle: 'ICLR 2023, spotlight'
+    }
+]
 
 export default class Publication extends Component{
     render(){
