84da2af09b9c1b754cffa53b8d4b657536fab424
jeongsol-kim <wjdtfh3927@gmail.com>
2023-02-26 17:30:36 +0900

    create news card

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -1,10 +1,40 @@
 
+import { Card, Divider, Grid, List, ListItem } from "@mui/material";
 import {Component, Fragment} from "react";
 
 
 const NewsBlock  = () => (
     <section className="section">
-        <p>Update!</p>
+        <div className="container is-max-desktop">
+        <div className="columns is-centered has-text-centered">
+            <div className="column is-four-fifths">
+                <Card>
+                    <Grid container>
+                        <Grid item>
+                            <h4 className="subtitle left">
+                            Update
+                            </h4>
+                        </Grid>
+                        <Divider orientation="vertical" flexItem />
+                        <Grid item>
+                            <List>
+                                <ListItem>
+                                    (2/6) Got Samsung Huantech gold award
+                                </ListItem>
+                                <ListItem>
+                                    (1/21) A paper accepted to ICLR 2023
+                                </ListItem>
+                                <ListItem>
+                                    (12/6) Publish a new repository for phase retrieval algorithems
+                                </ListItem>
+                            </List>
+                        </Grid>
+                    </Grid>
+                </Card>
+
+            </div>
+        </div>
+        </div>
     </section>
 )
 
