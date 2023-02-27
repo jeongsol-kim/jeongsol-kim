d9a807d11d90fd83da408cbffa4044698fb4b4ef
jeongsol-kim <wjdthf3927@gmail.com>
2023-02-27 23:43:39 +0900

    apply responsive to menu

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -2,36 +2,104 @@
 import { Button, IconButton, Toolbar, Typography} from "@mui/material";
 import Box from "@mui/material/Box";
 import AppBar from "@mui/material/AppBar";
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Fade from '@mui/material/Fade';
 import {Component, Fragment} from "react";
+import * as React from "react";
+import { useMediaQuery } from "react-responsive";
+import MenuIcon from '@mui/icons-material/Menu';
+
+export const Mobile = ({children}) => {
+    const isMobile = useMediaQuery({
+        query: "(max-width:768px)"
+    });
+
+    return <>{isMobile && children}</>
+}
+
+export const PC = ({children}) => {
+    const isPc = useMediaQuery({
+        query: "(min-width:769px)"
+    });
+
+    return <>{isPc && children}</>
+}
+
+export function FadeMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="fade-button"
+        aria-controls={open ? 'fade-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        <MenuIcon style={{color:'white'}}/>
+      </Button>
+      <Menu
+        id="fade-menu"
+        MenuListProps={{
+          'aria-labelledby': 'fade-button',
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Fade}
+      >
+        <MenuItem onClick={handleClose}>About</MenuItem>
+        <MenuItem onClick={handleClose}>Education</MenuItem>
+        <MenuItem onClick={handleClose}>Update</MenuItem>
+        <MenuItem onClick={handleClose}>Publication</MenuItem>
+        <MenuItem onClick={handleClose}>Award</MenuItem>
+      </Menu>
+    </div>
+  );
+}
 
 const NaviBlock2 = () => (
         <Box sx={{flexGrow: 1}}>
             <AppBar position='fixed' style={{backgroundColor:"#03C988"}}>
-                <Toolbar sx={{mx: "auto", width:"100%", maxWidth: 800}}>
+                <Toolbar sx={{mx: "auto", width:"100%", maxWidth: 800, justifyContent: "space-around"}}>
                     <IconButton size="large"
                                 edge="start"
                                 color="inherit"
                                 aria-label="menu"
-                                sx={{mr: 2}}>
+                                sx={{mr: 15}}>
                     <Typography variant="h6" component="div" sx={{flexGlow:1}}>
                         Jeongsol Kim
                     </Typography>
                     </IconButton>
-                    <Button color="inherit">
-                        About
-                    </Button>
-                    <Button color="inherit">
-                        Education
-                    </Button>
-                    <Button color="inherit">
-                        Update
-                    </Button>
-                    <Button color="inherit">
-                        Publications
-                    </Button>
-                    <Button color="inherit">
-                        Awards
-                    </Button>
+                    <PC>
+                        <Button color="inherit">
+                            About
+                        </Button>
+                        <Button color="inherit">
+                            Education
+                        </Button>
+                        <Button color="inherit">
+                            Update
+                        </Button>
+                        <Button color="inherit">
+                            Publications
+                        </Button>
+                        <Button color="inherit">
+                            Awards
+                        </Button>
+                    </PC>
+                    <Mobile>
+                        <FadeMenu />
+                    </Mobile>
                 </Toolbar>
             </AppBar>
         </Box>
