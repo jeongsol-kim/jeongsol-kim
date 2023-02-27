45cf63d8e90bf094d7fb90b4fd7a66f4b45c769b
jeongsol-kim <wjdthf3927@gmail.com>
2023-02-27 14:01:38 +0900

    tab / change timeline layout / change timeline line color

--- src/components/education/Education.js
+++ src/components/education/Education.js
@@ -6,48 +6,53 @@ import SchoolIcon from '@mui/icons-material/School';
 
 
 const TimeLine = () => (
-    <VerticalTimeline>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--education"
-    contentStyle={{ background: '#03C988', color: '#fff' }}
-    contentArrowStyle={{ borderRight: '7px solid #03C988' }}
-    date="2022 - present"
-    iconStyle={{ background: '#03C988', color: '#fff' }}
-    icon={<SchoolIcon />}
-  >
-    <h3 className="vertical-timeline-element-title">Bio and Brain Engineering</h3>
-    <h4 className="vertical-timeline-element-subtitle">Ph.D candidate</h4>
-    <p>
-      Korea Advanced Inst. of Science and Technology (KAIST)
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--education"
-    date="2020 - 2022"
-    iconStyle={{ background: '#BBBBBB', color: '#fff' }}
-    icon={<SchoolIcon />}
-  >
-    <h3 className="vertical-timeline-element-title">Bio and Brain Engineering</h3>
-    <h4 className="vertical-timeline-element-subtitle">M.S</h4>
-    <p>
-      Korea Advanced Inst. of Science and Technology (KAIST)
-    </p>
-    <p>
-        Supervisor: Jong Chul Ye
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--education"
-    date="2015 - 2020"
-    iconStyle={{ background: '#BBBBBB', color: '#fff' }}
-    icon={<SchoolIcon />}
-  >
-    <h3 className="vertical-timeline-element-title">Bio and Brain Engineering</h3>
-    <h4 className="vertical-timeline-element-subtitle">B.S (Magna Cum Laude)</h4>
-    <p>
-      Korea Advanced Inst. of Science and Technology (KAIST)
-    </p>
-  </VerticalTimelineElement>
+    <VerticalTimeline
+      layout="1-column-left"
+      lineColor="#BBBBBB">
+      <VerticalTimelineElement
+        className="vertical-timeline-element--education"
+        contentStyle={{ background: '#03C988', color: '#fff' }}
+        contentArrowStyle={{ borderRight: '7px solid #03C988' }}
+        date="2022 - present"
+        iconStyle={{ background: '#03C988', color: '#fff' }}
+        icon={<SchoolIcon />}
+        position="right"
+      >
+        <h3 className="vertical-timeline-element-title">Bio and Brain Engineering</h3>
+        <h4 className="vertical-timeline-element-subtitle">Ph.D candidate</h4>
+        <p>
+          Korea Advanced Inst. of Science and Technology (KAIST)
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--education"
+        date="2020 - 2022"
+        iconStyle={{ background: '#BBBBBB', color: '#fff' }}
+        icon={<SchoolIcon />}
+        position="right"
+      >
+        <h3 className="vertical-timeline-element-title">Bio and Brain Engineering</h3>
+        <h4 className="vertical-timeline-element-subtitle">M.S</h4>
+        <p>
+          Korea Advanced Inst. of Science and Technology (KAIST)
+        </p>
+        <p>
+            Supervisor: Jong Chul Ye
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--education"
+        date="2015 - 2020"
+        iconStyle={{ background: '#BBBBBB', color: '#fff' }}
+        icon={<SchoolIcon />}
+        position="right"
+      >
+        <h3 className="vertical-timeline-element-title">Bio and Brain Engineering</h3>
+        <h4 className="vertical-timeline-element-subtitle">B.S (Magna Cum Laude)</h4>
+        <p>
+          Korea Advanced Inst. of Science and Technology (KAIST)
+        </p>
+      </VerticalTimelineElement>
 </VerticalTimeline>    
 )
 
