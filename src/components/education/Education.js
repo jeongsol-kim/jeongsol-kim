0a988f324b79cb047862b5191040c3f0648a584b
jeongsol-kim <wjdthf3927@gmail.com>
2023-02-27 13:41:48 +0900

    change timeline design / remaining bugs

--- src/components/education/Education.js
+++ src/components/education/Education.js
@@ -2,6 +2,54 @@ import {Card} from "@mui/material"
 import {Component, Fragment} from "react";
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
+import SchoolIcon from '@mui/icons-material/School';
+
+
+const TimeLine = () => (
+    <VerticalTimeline>
+  <VerticalTimelineElement
+    className="vertical-timeline-element--education"
+    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+    date="2022 - present"
+    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    icon={<SchoolIcon />}
+  >
+    <h3 className="vertical-timeline-element-title">Bio and Brain Engineering</h3>
+    <h4 className="vertical-timeline-element-subtitle">Ph.D candidate</h4>
+    <p>
+      Korea Advanced Inst. of Science and Technology (KAIST)
+    </p>
+  </VerticalTimelineElement>
+  <VerticalTimelineElement
+    className="vertical-timeline-element--education"
+    date="2020 - 2022"
+    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    icon={<SchoolIcon />}
+  >
+    <h3 className="vertical-timeline-element-title">Bio and Brain Engineering</h3>
+    <h4 className="vertical-timeline-element-subtitle">M.S</h4>
+    <p>
+      Korea Advanced Inst. of Science and Technology (KAIST)
+    </p>
+    <p>
+        Supervisor: Jong Chul Ye
+    </p>
+  </VerticalTimelineElement>
+  <VerticalTimelineElement
+    className="vertical-timeline-element--education"
+    date="2015 - 2020"
+    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    icon={<SchoolIcon />}
+  >
+    <h3 className="vertical-timeline-element-title">Bio and Brain Engineering</h3>
+    <h4 className="vertical-timeline-element-subtitle">B.S</h4>
+    <p>
+      Korea Advanced Inst. of Science and Technology (KAIST)
+    </p>
+  </VerticalTimelineElement>
+</VerticalTimeline>    
+)
 
 const EducationBlock  = () => (
     <section className="section">
@@ -12,50 +60,7 @@ const EducationBlock  = () => (
                         <h2 style={{margin: '1em 1em', textAlign:'left'}}>
                             Education
                         </h2>
-                        <VerticalTimeline>
-                            <VerticalTimelineElement
-                                className="vertical-timeline-element--education"
-                                contentStyle={{background: 'rgb(33, 150, 243)', color: '#fff'}}
-                                contentArrowStyle={{borderRight: '7px solid rgb(33, 150, 243)'}}
-                                date="2015-2020"
-                                iconStyle={{background: 'rgb(33, 150, 243)', color:'#fff'}}
-                                >
-                                <h3 className="vertical-timeline-element-title">
-                                    B.S / Bio and Brain Engineering
-                                </h3>
-                                <h4 className="vertical-timeline-element-subtitle">
-                                    Korea Advanced Inst. of Science and Technology (KAIST)
-                                </h4>
-                            </VerticalTimelineElement>
-                            <VerticalTimelineElement
-                                className="vertical-timeline-element--education"
-                                contentArrowStyle={{borderRight: '7px solid rgb(33, 150, 243)'}}
-                                date="2020-2022"
-                                iconStyle={{background: 'rgb(33, 150, 243)', color:'#fff'}}
-                                >
-                                <h3 className="vertical-timeline-element-title">
-                                    M.S / Bio and Brain Engineering
-                                </h3>
-                                <h4 className="vertical-timeline-element-subtitle">
-                                    Korea Advanced Inst. of Science and Technology (KAIST)
-                                </h4>
-                                <p>Supervisor: Prof. Jong Chul Ye</p>
-                            </VerticalTimelineElement>
-                            <VerticalTimelineElement
-                                className="vertical-timeline-element--education"
-                                contentStyle={{background: 'rgb(33, 150, 243)', color: '#fff'}}
-                                contentArrowStyle={{borderRight: '7px solid rgb(33, 150, 243)'}}
-                                date="2022 - present"
-                                iconStyle={{background: 'rgb(33, 150, 243)', color:'#fff'}}
-                                >
-                                <h3 className="vertical-timeline-element-title">
-                                    Ph.D candidate / Bio and Brain Engineering
-                                </h3>
-                                <h4 className="vertical-timeline-element-subtitle">
-                                    Korea Advanced Inst. of Science and Technology (KAIST)
-                                </h4>
-                            </VerticalTimelineElement>
-                        </VerticalTimeline>
+                        <TimeLine />
                     </Card>
                 </div>
             </div>
