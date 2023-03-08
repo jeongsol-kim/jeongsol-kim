9b3d3dc297376a8bef724759c1cd93d2f92def1b
jeongsol-kim <wjdthf3927@gmail.com>
2023-03-08 12:21:40 +0000

    add award

--- src/components/award/Award.js
+++ src/components/award/Award.js
@@ -1,4 +1,5 @@
-import {Card} from "@mui/material";
+import {Avatar, Card, List, ListItem, ListItemAvatar, ListItemText} from "@mui/material";
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
 import {forwardRef, Fragment} from "react";
 
 
@@ -11,12 +12,42 @@ const AwardBlock = forwardRef((props, ref) => (
                         <h2 style={{margin: '1em 1em', textAlign:'left'}}>
                             Honors & Awards
                         </h2>
+                        <List>
+                            {itemData.map((item) => (
+                            <ListItem>
+                                <ListItemAvatar>
+                                <Avatar>
+                                    <EmojiEventsIcon />
+                                </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText 
+                                    primary={item.title}
+                                    secondary={item.subtitle}
+                                />
+                            </ListItem>
+                            ))}
+                        </List>
                     </Card>
                 </div>
             </div>
         </div>
     </section>
 ));
+
+const itemData = [
+    {
+        title: "Samsung Humantech Paper Award - Gold award",
+        subtitle: '1st in signal processing / 2023.02.06'
+    },
+    {
+        title: "2019 BBE Excellent Undergraduate Research",
+        subtitle: "BBE, KAIST / 2020.08.28"
+    },
+    {
+        title: "Dean's List in recognition of outstanding scholastic achievement",
+        subtitle: "College of enginerring, KAIST / 2017 spring and fall"
+    }
+]
 
 const Awards = forwardRef((props, ref) => {
     return (
