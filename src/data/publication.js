a2dc8cfa2e6318707a102051a45e763cfe2da4e0
jeongsol-kim <wjdthf3927@gmail.com>
2023-04-16 19:01:00 +0900

    change paper thumbnail path

--- src/data/publication.js
+++ src/data/publication.js
@@ -1,8 +1,6 @@
-const prefix = '../assets/publications/'
-
 export const publicationData = [
     {
-        img: prefix + 'blinddps',
+        img: process.env.PUBLIC_URL + '/imgs/blinddps',
         title: 'Parallel Diffusion Models of Operator and Image for Bilnd Inverse Problems',
         where: 'Hyungjin Chung*, Jeongsol Kim*, Sehui Kim, Jong Chul Ye (*co-first) / CVPR 2023',
         code: '',
@@ -10,7 +8,7 @@ export const publicationData = [
 
     },
     {
-        img: prefix + 'dps',
+        img: process.env.PUBLIC_URL + '/imgs/dps',
         title: 'Diffusion Posterior Sampling for General Noisy Inverse Problems',
         where: 'Hyungjin Chung*, Jeongsol Kim*, Michael T.McCann, Marc L. Klasky, Jong Chul Ye (*co-first) / ICLR 2023, spotlight',
         code: 'https://github.com/DPS2022/diffusion-posterior-sampling',
@@ -18,21 +16,21 @@ export const publicationData = [
         
     },
     {
-        img: prefix + 'tavit',
+        img: process.env.PUBLIC_URL + '/imgs/tavit',
         title: 'Task-Agnostic Vision Transformer for Distributed Learning of Image Processing',
         where: 'Boah Kim*, Jeongsol Kim*, Jong Chul Ye (*co-first) / IEEE Transaction on Image Processing, Vol 32',
         code: 'https://github.com/TAViT2022/TAViT',
         paper: 'https://ieeexplore.ieee.org/document/9979765',
     },
     {
-        img: prefix + 'festa',
+        img: process.env.PUBLIC_URL + '/imgs/festa',
         title: 'Federated Split Vision Transformer for COVID-19 CXR Diagnosis using Task-Agnostic Training',
         where: 'Sangjoon Park*, Gwanghun Kim*, Jeongsol Kim, Boah Kim, Jong Chul Ye (*co-first) / NeurIPS 2021',
         code: '',
         paper: 'https://openreview.net/pdf?id=Ggikq6Tdxch',
     },
     {
-        img: prefix + 'otcyclegan',
+        img: process.env.PUBLIC_URL + '/imgs/otcyclegan',
         title: 'Optimal Transport Driven CycleGAN for Unsupervised Learning in Inverse Problem',
         where: 'Byeongsu Sim, Gyutaek Oh, Jeongsol Kim, Chanyonh Jung, Jong Chul Ye / SIAM Journal on Imaging Science Vol. 13 (2020)',
         code: '',
