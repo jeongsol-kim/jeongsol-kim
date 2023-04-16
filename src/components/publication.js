2c35457b76a069f0d9017b286caecd3232043f09
jeongsol-kim <wjdthf3927@gmail.com>
2023-04-16 18:44:38 +0900

    add publication list

--- src/components/publication.js
+++ src/components/publication.js
@@ -1,10 +1,10 @@
 
+import PublicationList from "./publicationList";
+import { publicationData } from "../data/publication";
 
 const Publication = () => {
     return (
-        <div>
-            <h2>Publication page</h2>
-        </div>
+        <PublicationList itemData={publicationData}/>
     );
 }
 
