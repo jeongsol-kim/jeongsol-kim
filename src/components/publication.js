556a777738370b3978ea5c9d89ff68a6ba2fcd23
jeongsol-kim <wjdthf3927@gmail.com>
2023-04-16 19:09:01 +0900

    add header

--- src/components/publication.js
+++ src/components/publication.js
@@ -4,7 +4,13 @@ import { publicationData } from "../data/publication";
 
 const Publication = () => {
     return (
+        <>
+        <h2 className="hello"
+            style={{textAlign:'center', padding: '1rem 0'}}>
+            Publications
+        </h2>
         <PublicationList itemData={publicationData}/>
+        </>
     );
 }
 
