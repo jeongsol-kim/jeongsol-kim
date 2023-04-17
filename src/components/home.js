0973d3a25940cf1215f3eaca181deeb7da00df91
jeongsol-kim <wjdthf3927@gmail.com>
2023-04-17 10:55:57 +0900

    remove unused import

--- src/components/home.js
+++ src/components/home.js
@@ -7,7 +7,6 @@ import Popover from '@mui/material/Popover';
 import Typography from '@mui/material/Typography';
 import { useState } from 'react';
 
-import Button from '@mui/material/Button';
 import Snackbar from '@mui/material/Snackbar';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
