4c3d2f809a602cf24071e6d6fb298b7d0266247e
jeongsol-kim <wjdthf3927@gmail.com>
2023-04-18 17:09:15 +0900

    apply markdown

--- src/components/sidework.js
+++ src/components/sidework.js
@@ -1,8 +1,12 @@
+import styled from "styled-components";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import post from "../data/post";
 
 const Sidework = () => {
     return (
-        <div>
-            <h2> Side workds </h2>
+        <div className="center-content">
+        <ReactMarkdown remarkPlugins={[remarkGfm]} children={post} />
         </div>
     );
 }
---
+++ src/data/post.js
@@ -0,0 +1,20 @@
+
+const post = `
+
+# Header
+
+**Bold**
+
+normal text
+
+\`\`\`
+    print("Hello, world")
+    print("this is code block")
+\`\`\`
+
+| test |
+|-|
+| 3.14 |
+`;
+
+export default post
