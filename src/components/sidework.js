dc6f57e5acead524248eb63e1253760806fd33f3
jeongsol-kim <wjdthf3927@gmail.com>
2023-04-19 06:10:00 +0900

    create imagelist

--- src/components/sidework.js
+++ src/components/sidework.js
@@ -1,26 +1,43 @@
-import PostPage from "./post_page";
+import { ImageList, ImageListItem, ImageListItemBar, Stack } from "@mui/material";
 import { Link, Route, Routes } from "react-router-dom";
+import PostPage from "./post_page";
 import { PostList } from "../data/post_list";
 
-const WorkList = ({match}) => (
-    <>
-    <h2> Test </h2>
-    <ul>
+const WorkBlock = ({item}) => {
+    return (
+        <ImageListItem key={item.id}>
+        <img
+            src={item.thumbnail}
+            alt={item.title}
+            loading='lzay' 
+        />
+        <ImageListItemBar 
+            title={item.title}
+            position="below"
+        />
+        </ImageListItem>
+    )
+}
+
+const WorkList = () => (
+    <ImageList cols={2} gap={'2rem'}>
         {PostList.map((item) => (
-            <li key={item.title}>
-                <Link to={`${item.title}`}>
-                    {item.title}
-                </Link> 
-            </li>
-        ))}
-    </ul>
-    </>
+            <Link to={`${item.title}`}>
+                <WorkBlock item={item} />
+            </Link> 
+        ))
+        }
+    </ImageList>
 )
 
 
 const Sidework = () => {
     return (
         <div className="center-content">
+            <h2 className="hello"
+                style={{textAlign:'center', padding: '1rem 0'}}>
+                Side Projects
+            </h2>
             <Routes>
             <Route path='/' element={<WorkList />} />
             <Route path={':title'} element={<PostPage />} />
