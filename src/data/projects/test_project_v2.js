4ed2c4dd8db78a20ecf5256e51fabe0642d0111b
jeongsol-kim <wjdthf3927@gmail.com>
2023-04-19 05:57:33 +0900

    add temporary files for post

---
+++ src/data/projects/test_project_v2.js
@@ -0,0 +1,21 @@
+
+const post = `
+
+# Post 2
+
+**Bold**
+
+normal text
+
+\`\`\`
+    print("Hello, world")
+    print("this is code block")
+\`\`\`
+
+| test |
+|-|
+| 3.14 |
+`;
+
+const _post = post;
+export { _post as post };
---
+++ src/data/projects/test_v1.md
@@ -0,0 +1,14 @@
+# Header
+
+**Bold**
+
+normal text
+
+\`\`\`
+    print("Hello, world")
+    print("this is code block")
+\`\`\`
+
+| test |
+|-|
+| 3.14 |
