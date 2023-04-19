3ec46e4e8ae1b951816025812adb14a0a55d8e4f
jeongsol-kim <wjdthf3927@gmail.com>
2023-04-20 00:34:07 +0900

    add bash script that generate post metadata

---
+++ src/create.sh
@@ -0,0 +1,35 @@
+#/bin/bash
+
+# output file
+file=src/data/post_list.js
+# clear file
+> ${file}
+# create pre-javascripts
+echo -e "export const PostList = [" >> ${file}
+
+# create metadata for posts
+i=0
+for post in "src/data/write"/*.md
+do
+    # get title (read first line of md file)
+    title=$(head -1 ${post})
+    # assume that the title is written by #.
+    # strip # and a whitespace
+    title="${title:2}"
+
+    # get thumbnail src
+
+    # get basename
+    basename="${post##*/}"  # cut longest matching with */ from beginning
+    basename="${basename%.*}"  # cut shortest matching with .* from end
+
+    prompt="{\n\tid: $i,\n\tpath: './projects/$basename',\
+            \n\ttitle: '$title',\
+            \n\tthumbnail: process.env.PUBLIC_URL + '/logo512.png',\
+            \n\tcontent: require('./projects/$basename').post\n},"
+    echo -e ${prompt} >> ${file}
+    i=$(( i + 1 ))
+done
+
+# create post-javascripts
+echo -e "];" >> ${file}
