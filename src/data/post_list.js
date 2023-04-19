4d7a8445191ffbdc9426aa8e97aa0bc137383efd
jeongsol-kim <wjdthf3927@gmail.com>
2023-04-20 00:34:49 +0900

    remove redundant prefix

--- src/data/post_list.js
+++ src/data/post_list.js
@@ -1,28 +1,23 @@
-const prefix = '../data/projects/'
-const full_path = (name) => (
-    prefix + name
-)
-
 export const PostList = [
-    {
-        id: 0,
-        path: full_path('test_v1'),
-        title: 'Simple Implementation of JAX-LAX',
-        thumbnail: process.env.PUBLIC_URL + '/logo512.png',
-        content: require('./projects/test_v1').post
-    },
-    {
-        id: 1,
-        path: full_path('test_v2'),
-        title: 'Test project Version 2',
-        thumbnail: process.env.PUBLIC_URL + '/logo512.png',
-        content: require('./projects/test_v2').post
-    },
-    {
-        id: 2,
-        path: full_path('test_v3'),
-        title: 'Test project Version 3',
-        thumbnail: process.env.PUBLIC_URL + '/logo512.png',
-        content: require('./projects/test_v3').post
-    },
+{
+	id: 0,
+	path: './projects/test_v1', 
+	title: 'Simple JAX-FLAX implementation', 
+	thumbnail: process.env.PUBLIC_URL + '/logo512.png', 
+	content: require('./projects/test_v1').post
+},
+{
+	id: 1,
+	path: './projects/test_v2', 
+	title: 'Header', 
+	thumbnail: process.env.PUBLIC_URL + '/logo512.png', 
+	content: require('./projects/test_v2').post
+},
+{
+	id: 2,
+	path: './projects/test_v3', 
+	title: 'Header', 
+	thumbnail: process.env.PUBLIC_URL + '/logo512.png', 
+	content: require('./projects/test_v3').post
+},
 ];
