6729033d469cc5188d9c6ccec2e3bbe167e54a83
jeongsol-kim <wjdthf3927@gmail.com>
2023-04-20 00:34:28 +0900

    separate md and js files

--- src/convert.sh
+++ src/convert.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-search_dir=src/data/projects
+search_dir=src/data/write
 for filename in "$search_dir"/*.md
 do
     jsfile="${filename/.md/.js}"
+    jsfile="${jsfile/write/projects}"
     echo -e "const post=\` \n $(cat ${filename})\`\n\n const _post = post;\n export { _post as post };" > ${jsfile}
 done
