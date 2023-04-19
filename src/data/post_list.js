fd4149eeba4b38e4109816e654928e2ee9f814bd
jeongsol-kim <wjdthf3927@gmail.com>
2023-04-19 19:39:28 +0900

    change post list

--- src/data/post_list.js
+++ src/data/post_list.js
@@ -6,23 +6,23 @@ const full_path = (name) => (
 export const PostList = [
     {
         id: 0,
-        path: full_path('test_project_v1'),
-        title: 'Test project Version 1',
+        path: full_path('test_v1'),
+        title: 'Simple Implementation of JAX-LAX',
         thumbnail: process.env.PUBLIC_URL + '/logo512.png',
-        content: require('./projects/test_project_v1').post
+        content: require('./projects/test_v1').post
     },
     {
         id: 1,
-        path: full_path('test_project_v2'),
+        path: full_path('test_v2'),
         title: 'Test project Version 2',
         thumbnail: process.env.PUBLIC_URL + '/logo512.png',
-        content: require('./projects/test_project_v2').post
+        content: require('./projects/test_v2').post
     },
     {
         id: 2,
-        path: full_path('test_project_v3'),
+        path: full_path('test_v3'),
         title: 'Test project Version 3',
         thumbnail: process.env.PUBLIC_URL + '/logo512.png',
-        content: require('./projects/test_project_v3').post
+        content: require('./projects/test_v3').post
     },
 ];
