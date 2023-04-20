703cbbf4087d0f680a3f97dd72f0e409112bebf9
jeongsol-kim <wjdthf3927@gmail.com>
2023-04-20 16:59:35 +0900

    add code block convert

--- src/convert.sh
+++ src/convert.sh
@@ -4,6 +4,11 @@ search_dir=src/data/write
 for filename in "$search_dir"/*.md
 do
     jsfile="${filename/.md/.js}"
+    # convert code block expression for js
+    content=$(cat ${filename})
+    content="${content//\`\`\`/\\\`\\\`\\\`}"
+    # set outpath
     jsfile="${jsfile/write/projects}"
-    echo -e "const post=\` \n $(cat ${filename})\`\n\n const _post = post;\n export { _post as post };" > ${jsfile}
+    echo $content
+    echo -e "const post=\` \n${content}\n\`\n\nconst _post = post;\nexport { _post as post };" > ${jsfile}
 done
