f572622b1144028a54cce895cb0a7ff2dc32587b
jeongsol-kim <wjdthf3927@gmail.com>
2023-04-20 20:29:14 +0900

    write new thing

--- src/data/post_list.js
+++ src/data/post_list.js
@@ -3,7 +3,7 @@ export const PostList = [
 	id: 0,
 	path: './projects/230420_jaxflax', 
 	title: 'Simple JAX-FLAX implementation', 
-	thumbnail: 'https://ichef.bbci.co.uk/news/976/cpsprodpb/17638/production/_124800859_gettyimages-817514614.jpg', 
+	thumbnail: 'https://modulabs.co.kr/wp-content/uploads/2023/02/jax_logo-1536x480.jpg', 
 	content: require('./projects/230420_jaxflax').post
 },
 ];
---
+++ upload.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# convert
+bash src/convert.sh
+
+# create data
+bash src/create.sh
+
+# git push
+git add .
+git commit -m "write new thing"
+git push origin dev
